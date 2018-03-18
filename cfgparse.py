2d397e80998e3e1f838907c5eddaeeeade7ef6a8
Colin Bounouar <Colin-b@users.noreply.github.com>
2018-03-18 20:38:03 +0100

    Handle the fact that section does not exists

--- cfgparse.py
+++ cfgparse.py
@@ -8,10 +8,10 @@ except ImportError:
 def load_config(file_path):
     """
     Load configuration parser from ini file.
-    Throw an Exception if file cannot be found.
     
     :param file_path: string path to the configuration ini file.
     :return: ConfigParser instance.
+    :raise Exception: If file cannot be found
     """
     config_parser = ConfigParser()
     if not config_parser.read(file_path):
@@ -21,22 +21,15 @@ def load_config(file_path):
 
 def get_item(config, section, key, *args):
     """
-    Return property value or throw an Exception if it cannot be found and no default value is provided.
+    Return property value.
     
     :param config: Configuration parser instance.
     :param section: Section name in configuration with property to look for.
     :param key: Name of the key to look for in section.
     :param args: Can contains the default value (consider as mandatory if nothing is provided).
     :return: Key string value.
+    :raise Exception: If key cannot be found and not default value is provided.
     """
-    try:
-        # Python 3
-        section_dict = config[section]
-        if key not in section_dict and not args:
-            raise Exception('"{0}" configuration section must provide "{1}".'.format(section, key))
-        return section_dict[key] if key in section_dict else args[0]
-    except AttributeError:
-        # Python 2
-        if not config.has_option(section, key) and not args:
-            raise Exception('"{0}" configuration section must provide "{1}".'.format(section, key))
-        return config.get(section, key) if config.has_option(section, key) else args[0]
+    if not config.has_option(section, key) and not args:
+        raise Exception('"{0}" configuration section must provide "{1}".'.format(section, key))
+    return config.get(section, key) if config.has_option(section, key) else args[0]
